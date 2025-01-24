c207738db67c49b6535b54499302bf2b509b8ebb
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2025-01-24 18:11:47 +0500

    new models created

--- legendsWear/core/admin.py
+++ legendsWear/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Category, FeaturedSection
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(FeaturedSection)
--- legendsWear/core/models.py
+++ legendsWear/core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+# Model 1 Categories
+# Categories ID
+# Categories Name
+class Category(models.Model):
+    category_id = models.CharField(max_length=10, unique=True, editable=False)
+    category_name = models.CharField(max_length=50)
+
+# Model 2 Products
+# Product ID   
+# Product Name 
+# Product Desc 
+# Product Image
+# Product associated with Category (show all drop down in categories)
+# Stock Availability (Boolean field)
+class Product(models.Model):
+    id = models.CharField()
+    name = models.CharField()
+    desc = models.TextField()
+    image = models.ImageField(upload_to="products/")
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name 
+    
+# Model 3  Featured section
+# Here all product will listed, user have to check desire product which want to show in featured section
+class FeaturedSection(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    # Many-to-Many relationship to associate multiple products with this featured section
+    featured_products = models.ManyToManyField(Product, related_name='featured_sections')
+    
+    def __str__(self):
+        return self.title
+
